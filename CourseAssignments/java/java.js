b5d896c4dd24d95edf2c3ce3521cd87650e225ac
Eetu Huotari <eetu142@gmail.com>
2022-12-12 15:31:08 +0200

    Made chatbox

--- CourseAssignments/java/java.js
+++ CourseAssignments/java/java.js
@@ -1,7 +1,7 @@
 // Creates an array and listens the button
 let AllMessageInformations = [];
 document.getElementById("submitMessage").addEventListener('click', submitMessage);
-let form = document.querySelector("form");
+let form = document.getElementById("messagesForm");
 
 // Function for the send message button
 function submitMessage(e) {
@@ -10,10 +10,9 @@ function submitMessage(e) {
     let formData = new FormData(form);
     let username = formData.get("username");
     let message = formData.get("message");
-    let important = formData.get("important");
 
     let messageInformation = {
-        username, message, important
+        username, message
     }
 
     AllMessageInformations.push(messageInformation);
@@ -26,7 +25,6 @@ function printMessages() {
     for (const messageInformation of AllMessageInformations) {
         console.log(messageInformation.username);
         console.log(messageInformation.message);
-        console.log(messageInformation.important);
     }
 
 }
